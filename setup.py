e7e4a727818c0bb9161588efd9064751eebce3eb
Tom de Geus <tdegeus@users.noreply.github.com>
2019-11-26 17:24:21 +0100

    Fixing setup (#7)

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name = 'pdfcombine',
-    description = 'Script of combine PDF files (wrapper around GhostScript)',
-    long_description = 'Script of combine PDF files (wrapper around GhostScript)',
-    keywords = 'PDF, GhostScript',
-    version = '0.4.0',
+    version = '0.4.2',
     license = 'MIT',
     author = 'Tom de Geus',
     author_email = 'tom@geus.me',
+    description = 'Script of combine PDF files (wrapper around GhostScript)',
+    long_description = 'Script of combine PDF files (wrapper around GhostScript)',
+    keywords = 'PDF, GhostScript',
     url = 'https://github.com/tdegeus/pdfcombine',
+    packages = ['pdfcombine'],
     entry_points = {
         'console_scripts': ['pdfcombine = pdfcombine:main']}
 )
