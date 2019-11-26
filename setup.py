8e0341579105fb419dd612218be59dbd7c4506a7
Tom de Geus <tom@geus.me>
2019-11-26 17:23:13 +0100

    Fixing installation

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     long_description = 'Script of combine PDF files (wrapper around GhostScript)',
     keywords = 'PDF, GhostScript',
     url = 'https://github.com/tdegeus/pdfcombine',
+    packages = ['pdfcombine'],
     entry_points = {
         'console_scripts': ['pdfcombine = pdfcombine:main']}
 )
