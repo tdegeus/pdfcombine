cb35122b969dd9f5a07ff79f3e4459823a510c13
Tom de Geus <tom@geus.me>
2019-11-26 17:24:09 +0100

    Bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'pdfcombine',
-    version = '0.4.1',
+    version = '0.4.2',
     license = 'MIT',
     author = 'Tom de Geus',
     author_email = 'tom@geus.me',
